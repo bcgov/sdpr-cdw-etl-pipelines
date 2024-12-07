384c9eff743ca3c35d4740563124c0ce2881aece
James Scott <james.b.scott@gov.bc.ca>
2024-12-06 16:53:41 -0800

    data-extract: reformatted

--- data-extract/run_data_extract.bat
+++ data-extract/run_data_extract.bat
@@ -2,6 +2,4 @@
 call E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\.venv\Scripts\activate.bat
 
 @REM run python job script
-python "E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data_extract_cmd_line.py" ^
-    E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\sample.sql ^ 
-    E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data.xlsx
+python "E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data_extract_cmd_line.py" E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\sample.sql E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data.xlsx
