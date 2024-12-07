b3ff826655c0eb0b694ac844f8b6fd95576478ca
James Scott <james.b.scott@gov.bc.ca>
2024-12-06 16:35:07 -0800

    refresh: added import schema function

--- refresh/refresh.py
+++ refresh/refresh.py
@@ -54,6 +54,34 @@ class Refresh:
         c = Cmd()
         c.run_cmd(cmd)
 
+    def import_schema_w_datapump(
+        self,
+        schemas,
+        remap_from_schema=None,
+        remap_to_schema=None,
+        to_db_user=os.getenv('TO_DB_USER'),
+        to_db_password=os.getenv('TO_DB_PASSWORD'),
+        to_db_connect_identifier=os.getenv('TO_DB_CONNECT_IDENTIFIER'),
+        dir=os.getenv('DATAPUMP_ENV_REFRESH_DIR'),
+        network_link=os.getenv('FROM_DB_LINK'),
+        table_exists_action='replace',
+    ):
+        # impdp command fragments
+        _destination_db = f'{to_db_user}/{to_db_password}@{to_db_connect_identifier}'
+        _directory = f'directory={dir}'
+        _network_link = f'network_link={network_link}'
+        _schemas = f'schemas={schemas}'
+        if remap_from_schema is None and remap_to_schema is None:
+            _remap = ''
+        else:
+            _remap = f'remap_schema={remap_from_schema}:{remap_to_schema}'
+        _table_exists_action = f'table_exists_action={table_exists_action}'
+
+        cmd = f'impdp "{_destination_db}" {_directory} {_network_link} {_schemas} {_remap} {_table_exists_action}'
+        logger.info(cmd)
+        c = Cmd()
+        c.run_cmd(cmd)
+
     def refresh_table_w_sqlplus(self, table):
         s = SqlPlus()
         s.create_or_replace_copy(
