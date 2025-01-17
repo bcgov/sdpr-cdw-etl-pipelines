b977d27ad0b60ce9aeb1cb372aed301d9ea2ee6d
James.Scott <james.b.scott@gov.bc.ca>
2025-01-17 13:54:57 -0800

    data_extract: created test folder

--- data-extract/run_data_extract.bat
+++
@@ -1,5 +0,0 @@
-@REM activate virtual environment
-call E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\.venv\Scripts\activate.bat
-
-@REM run python job script
-python "E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data_extract_cmd_line.py" E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\sample.sql E:\ETL_V8\sdpr-cdw-data-pipelines\data-extract\data.xlsx
