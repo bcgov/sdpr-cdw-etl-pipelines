1cfa612ddcc079d82f81d3c25ae9f6ac2b47db7f
James Scott <james.b.scott@gov.bc.ca>
2025-01-23 17:07:57 -0800

    msp enrollement extract: updated batch script

--- data-quality/msp_enrollement_extract/extract_msp_enrollement.bat
+++ data-quality/msp_enrollement_extract/extract_msp_enrollement.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-@SETLOCAL & PUSHD & SET RET=
+@SETLOCAL & PUSHD
 @REM ---------------------------------------------------------------
 @REM HCDWLPPA - populate SDD related ODS tables from ICM_STG.
 @REM
@@ -26,13 +26,12 @@ call E:\ETL_V8\sdpr-cdw-data-pipelines\data-quality\msp_enrollement_extract\.ven
 @REM run python job script
 python "E:\ETL_V8\sdpr-cdw-data-pipelines\data-quality\msp_enrollement_extract\extract_msp_enrollement.py"
 
-@echo python done
+echo python finished with exit code = %ERRORLEVEL% >>%BATCH_LOG_FILE%
 
-@SET EXIT_CODE=%RET%
-	echo EXIT_CODE is %EXIT_CODE% >>%BATCH_LOG_FILE%
-	echo RET is %RET% >>%BATCH_LOG_FILE%
+@SET EXIT_CODE=%ERRORLEVEL%
+echo EXIT_CODE is %EXIT_CODE% >>%BATCH_LOG_FILE%
 
-if %RET%==0 (
+if %ERRORLEVEL%==0 (
 	echo [%TIME%] %~n0 finished -- SUCCESS -- >>%BATCH_LOG_FILE% 
 ) else (
 	echo [%TIME%] %~n0 finished -- FAILURE -- >>%BATCH_LOG_FILE% 
