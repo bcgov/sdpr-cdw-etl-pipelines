6bf04b5c68a5bdc52c9d4f61b424486f4a341148
James Scott <james.b.scott@gov.bc.ca>
2025-01-24 10:50:02 -0800

    refresh: added chips_stg.ps_pay_check

---
+++ refresh/chips_stg/individual_tables/ps_pay_check.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+from dotenv import load_dotenv
+import os 
+load_dotenv(dotenv_path="E:\\ETL_V8\\sdpr-cdw-data-pipelines\\refresh\\.env")
+base_dir = os.getenv('MAIN_BASE_DIR')
+sys.path.append(base_dir)
+from refresh.refresh import Refresh
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    # filename=f'{this_dir}\\refresh.log',
+    # filemode='w',
+    level=logging.DEBUG, 
+    format="{levelname} ({asctime}): {message}", 
+    datefmt='%d/%m/%Y %H:%M:%S',
+    style='{'
+)
+
+def main():
+    table = 'chips_stg.ps_pay_check'
+    r = Refresh()
+    r.import_table_w_datapump(tables=table)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        logging.exception('Got exception on main handler')
+        raise
