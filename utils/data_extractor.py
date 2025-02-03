8f1a32afc113e9817acbb98d0c0b0878b6d619fd
James Scott <james.b.scott@gov.bc.ca>
2025-02-03 14:33:23 -0800

    utils: added timestamped file name functions to data_extractor

--- utils/data_extractor.py
+++ utils/data_extractor.py
@@ -1,6 +1,9 @@
 from utils.oracle_db import OracleDB
 import logging
 import pandas as pd
+import os
+import datetime as dt
+import shutil
 
 logger = logging.getLogger('__main__.' + __name__)
 
@@ -46,3 +49,73 @@ class DataExtractor:
                     index=False,
                 )   
                 query_num += 1
+    
+    def sql_to_xlsx_with_timestamp(
+        self, 
+        sql_filepath, 
+        xlsx_dir, 
+        xlsx_filename_before_timestamp, 
+        delete_old_timestamped_xlsx_files=False
+    ):
+        """
+        Runs SQL queries in an .sql file against an Oracle DB and returns the output of each query in 
+        an .xlsx file located in xlsx_dir and named in the form of 
+        "xlsx_filename_before_timestamp + timestamp.xlsx"
+
+        Args:
+            sql_filepath (str): the path to the .sql input file 
+            xlsx_dir (str): the directory to the xlsx output file
+            xlsx_filename_before_timestamp (str): the name of the xlsx file, which will be appended 
+                with a timestamp
+            delete_old_timestamped_xlsx_files (bool): True := will delete old timestamped versions of
+                the file. False (default) := will not.
+        """
+        now = dt.datetime.now()
+        timestamp = now.strftime("%Y-%m-%d %H-%M-%S")
+        xlsx_filename = xlsx_filename_before_timestamp + ' ' + timestamp + '.xlsx'
+        xlsx_filepath = xlsx_dir + xlsx_filename
+        self.sql_to_xlsx(sql_filepath, xlsx_filepath)
+        if delete_old_timestamped_xlsx_files:
+            self.delete_old_timestamped_xlsx_files(
+                xlsx_dir, xlsx_filename_before_timestamp, xlsx_filename
+            )
+
+    def delete_old_timestamped_xlsx_files(
+        self, xlsx_dir, xlsx_filename_before_timestamp, xlsx_filename_w_curr_timestamp
+    ):
+        """
+        Deletes timestamped versions of an xlsx file that aren't the latest one.
+
+        Args:
+            xlsx_dir (str): the directory to the xlsx file(s)
+            xlsx_filename_before_timestamp (str): the names of the xlsx file(s) excluding
+                the timestamp that's appended to the end
+            xlsx_filename_w_curr_timestamp: the timestamped name of the current xlsx file
+                to be left as is
+        """
+        for (dirpath, dirnames, filenames) in os.walk(xlsx_dir):
+            filenames = filenames
+        to_index = len(xlsx_filename_before_timestamp)
+        for filename in filenames:
+            if filename[:to_index] == xlsx_filename_before_timestamp \
+                and len(filename) == len(xlsx_filename_w_curr_timestamp) \
+                and filename != xlsx_filename_w_curr_timestamp:
+                try:
+                    os.remove(xlsx_dir + filename)
+                except PermissionError:
+                    logger.info(f'encountered PermissionError when trying to remove old file version: {filename}')
+                    continue
+
+    def copy_file(self, src, dst):
+        """
+        Copies files from src to dst unless a PermissionError is encountered, in which case
+        it does nothing.
+
+        Args:
+            src (str): the source file path
+            dst (str): the destination file path
+        """
+        try:
+            shutil.copy(src = src, dst = dst)
+        except PermissionError:
+            pass
