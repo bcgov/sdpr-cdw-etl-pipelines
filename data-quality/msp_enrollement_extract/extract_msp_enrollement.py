9f4d03cfff3be1ce2d229f789ce8372674e8a235
James Scott <james.b.scott@gov.bc.ca>
2025-02-03 14:33:55 -0800

    msp enrollment: setup timestamped naming to avoid PermissionError

--- data-quality/msp_enrollement_extract/extract_msp_enrollement.py
+++ data-quality/msp_enrollement_extract/extract_msp_enrollement.py
@@ -1,19 +1,20 @@
 import logging
-import getpass
 import sys
 from dotenv import load_dotenv
 import os 
 load_dotenv()
 base_dir = os.getenv('MAIN_BASE_DIR')
+esp_log_file_dir = os.getenv('ESP_LOG_FILE_DIR')
 sys.path.append(base_dir)
 from utils.oracle_db import OracleDB
 from utils.data_extractor import DataExtractor
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
+log_filepath = fr'{this_dir}\extract_msp_enrollement.log'
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    filename=fr'{this_dir}\extract_msp_enrollement.log',
+    filename=log_filepath,
     filemode='w',
     level=logging.DEBUG, 
     format="{levelname} ({asctime}): {message}", 
@@ -21,13 +22,16 @@ logging.basicConfig(
     style='{'
 )
 
+sql_file_path = fr'{this_dir}\dq_msp_enrollment.sql'
+xlsx_file_path = r'//sfp.idir.bcgov/s134/s34404/GetDoc/CDW-SDPR/DQ TL/MSP Enrollment ID by Case Number Report.xlsx'
+xlsx_dir = r'//sfp.idir.bcgov/s134/s34404/GetDoc/CDW-SDPR/DQ TL/'
+xlsx_filename_before_timestamp = r'MSP Enrollment ID by Case Number Report'
+
 if __name__ == "__main__":
-    sql_file_path = fr'{this_dir}\dq_msp_enrollment.sql'
-    xlsx_file_path = r'//sfp.idir.bcgov/s134/s34404/GetDoc/CDW-SDPR/DQ TL/MSP Enrollment ID by Case Number Report.xlsx'
     try:
         db = OracleDB(conn_str_key_endpoint=os.getenv('ORACLE_CONN_STRING_KEY'))
         data_extractor = DataExtractor(oracle_db=db)
-        data_extractor.sql_to_xlsx(sql_file_path, xlsx_file_path)
+        data_extractor.sql_to_xlsx_with_timestamp(sql_file_path, xlsx_dir, xlsx_filename_before_timestamp)
         logger.info(fr'finished the sql to xlsx extraction from {sql_file_path} to {xlsx_file_path}')
     except:
         logging.exception('Got exception on main handler')
