23d0fad2db8f32f70b276ae688f4b6394a36938b
James Scott <james.b.scott@gov.bc.ca>
2025-02-04 11:59:37 -0800

    chips src to stg: moved etl to build catalog tables into folders and created batch scripts to activate venv and run script

---
+++ chips/chips-src-to-stg/etl_jobs/open_api_catalog_etl/api_catalog_endpoints/api_catalog_endpoints_etl.bat
@@ -0,0 +1,5 @@
+@REM activate virtual environment
+call E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips-src-to-stg\.venv\Scripts\activate.bat
+
+@REM run python job script
+python "E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips-src-to-stg\etl_jobs\open_api_catalog_etl\api_catalog_endpoints_etl.py"
